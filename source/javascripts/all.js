d5c377ac36487913bb2a2a45dcc28f4abef13837
rtaibah <taibah@gmail.com>
2014-04-22 13:03:34 +0300

    full width orbit implmented

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,16 +1,19 @@
 //= require jquery
-//= require_tree .
 //= foundation.min.js
-
+//= require_tree .
 $(document).foundation({
     orbit: {
         animation: 'slide',
-        timer_speed: 1000,
+        timer_speed: 0,
         pause_on_hover: true,
         resume_on_mouseout: true,
         animation_speed: 500,
         navigation_arrows: false,
         bullets: false,
-        next_on_click: true
+        next_on_click: true,
+        timer: false,
+        navigation_arrows: true,
+        slide_number: false,
                 }
            });
+
